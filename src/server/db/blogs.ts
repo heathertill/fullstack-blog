a2adcb885ecbd61d6b35ad2ae2be7a720abfa1d2
Heather Till <heather.till@charter.net>
2019-06-04 16:22:53 -0500

    -routes are hooked up and working in postman

--- src/server/db/blogs.ts
+++ src/server/db/blogs.ts
@@ -1,12 +1,19 @@
 import { Query } from './index';
 
-const all = async () => Query('SELECT * FROM blogs');
+const all = async () => Query('SELECT * FROM blogs ORDER BY _created DESC');
 
 const one = async (id: number) => Query('SELECT * FROM blogs WHERE id = ?', [id]);
 
+const createBlog = async (title: string, content: string, authorid: number) => Query('INSERT INTO blogs (title, content, authorid) VALUES (?, ?, ?)', [title, content, authorid]);
 
+const updateBlog = async (title: string, id: number) => Query('UPDATE blogs SET title = ? WHERE id = ?', [title, id]);
+
+const deleteBlog = async (id: number) => Query('DELETE FROM blogs WHERE id = ?', [id]);
 
 export default {
     all,
-    one
+    one,
+    createBlog,
+    updateBlog,
+    deleteBlog
 }
--- src/server/routes.ts
+++ src/server/routes.ts
@@ -4,10 +4,6 @@ import db from './db';
 
 const router = express.Router();
 
-router.get('/api/hello', (req, res, next) => {
-    res.json('World');
-});
-
 router.get('/api/blogs/:id?', async (req, res) => {
     let id = req.params.id
     if (id) {
@@ -15,18 +11,46 @@ router.get('/api/blogs/:id?', async (req, res) => {
             // the [0] gets the blog object from the array
             res.json((await db.Blogs.one(id))[0]);
         } catch (err) {
-            console.log('blogs/id: err:', err);
+            console.log(err);
             res.sendStatus(500);
         }
     } else {
         try {
             res.json(await db.Blogs.all());
         } catch (err) {
-            console.log('blogs err:', err);
+            console.log(err);
             res.sendStatus(500);
         }
     }
+});
 
+router.post('/api/blogs', async (req, res) => {
+    try {
+        let newBlog = await db.Blogs.createBlog(req.body.title, req.body.content, req.body.authorid);
+        res.json(newBlog);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+});
+
+router.put('/api/blogs/:id', async (req, res) => {
+    try {
+        res.json(await db.Blogs.updateBlog(req.body.title, req.params.id))
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+});
+
+router.delete('/api/blogs/:id', async(req, res) => {
+    try {
+        res.json(await db.Blogs.deleteBlog(req.params.id))
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
 })
+
 
 export default router;
