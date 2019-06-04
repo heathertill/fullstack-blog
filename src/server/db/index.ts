e9db05b5fdfd9fb3ef74d023ea2025a957ceb046
Heather Till <heather.till@charter.net>
2019-06-04 09:38:34 -0500

    -server protected and connected

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,18 +1,27 @@
 import * as mysql from 'mysql';
+let config = require('../../../config');
+let Connection = config.Connection;
+
 
 import Blogs from './blogs';
 
-export const Connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'dbblogsapp',
-    password: 'blahblah',
-    database: 'dbBlogs'
-});
+// export const Connection = mysql.createConnection({
+//         host: 'localhost',
+//         port: 3306,
+//         user: 'dbblogsapp',
+//         password: 'blahblah',
+//         database: 'dbBlogs'
+// });
 
 export const Query = (query: string, values?: Array<string | number>) => {
     return new Promise<Array<any>>((resolve, reject) => {
-        Connection.query(query, values, (err, results) => {
+        Connection.query(query, values, (err: any, results: any) => {
+            // if (err) {
+            //     console.log('conerr:', err);
+            //     reject(err)
+            // } else {
+            //     resolve(results)
+            // }
             if (err) return reject(err);
             return resolve(results);
     })
--- src/server/routes.ts
+++ src/server/routes.ts
@@ -15,14 +15,14 @@ router.get('/api/blogs/:id?', async (req, res) => {
             // the [0] gets the blog object from the array
             res.json((await db.Blogs.one(id))[0]);
         } catch (err) {
-            console.log(err);
+            console.log('blogs/id: err:', err);
             res.sendStatus(500);
         }
     } else {
         try {
             res.json(await db.Blogs.all());
         } catch (err) {
-            console.log(err);
+            console.log('blogs err:', err);
             res.sendStatus(500);
         }
     }
