d9a4984ec5730e93bf01c4cd5e8f84df8352a27d
Heather Till <heather.till@charter.net>
2019-06-05 19:41:52 -0500

    -updated last save

--- src/client/components/AddBlog.tsx
+++ src/client/components/AddBlog.tsx
@@ -2,13 +2,50 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-export interface AddBlogProps {
+import { Blog } from './Allblogs';
 
-}
+export interface AddBlogProps { }
 
 const AddBlog: React.SFC<AddBlogProps> = () => {
+
+    const [blog, useBlog] = useState<Blog>({});
+
+    const createBlog = async (name: string, title: string, content: string) => {
+        try {
+            
+        } catch (err) {
+            console.log(err)
+        }
+
+    }
+
+
+
+
+    // let data = { userid: this.state.userid, text: this.state.text };
+    // let res = await fetch('/api/chirps/', {
+    //     method: 'POST',
+    //     body: JSON.stringify(data),
+    //     headers: {
+    //         "Content-type": "application/json"
+    //     },
+    // });
+    // let info = await res.json();
+    // this.setState({ chirpId: info.insertId })
+
     return (
-        <div></div>
+                <div className="row">
+            <div className="c0l-md-4 mx-5">
+                <div className="card border border-dark rounded">
+                    <div className="card-body" key={blog.id}>
+                        <h5 className="card-title">{blog.title}</h5>
+                        <p className="card-text">{blog.content}</p>
+                        <p className="card-text">{blog._created}</p>
+                        <button onClick={() => history.goBack()} className="btn btn-warning shadow btn-block mx-auto">Go Back</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
